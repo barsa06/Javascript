a892c51d3b1f34f746f15c708cb91c2c3daa61a1
unknown <bpun7891@gmail.com>
2025-11-05 16:45:03 +0545

    added if else condition

---
+++ Index.js
@@ -0,0 +1,56 @@
+//variable declaration
+/*let name = "Rubi";
+name="sulisha";
+console.log(name);
+
+//array 
+const fruits = ["Apple", "Banana", "Carrot"];
+console.log(fruits);
+
+//objects
+const person = {
+    firstName: "John";
+}
+    */
+
+function isEven(a){
+      if (a%2==0) {
+        console.log("Even");
+      } else {
+        console.log("odd");
+      }
+}
+isEven(10)
+
+
+function whichGreater(num1, num2){
+    if (num1>num2){
+        console.log("Number 1 is greatest");
+    }
+    else {
+        console.log("Number 2 is greatest");
+    }
+
+}
+whichGreater(2,3)
+
+/*function whichGreater(num1, num2, num3) {
+    if(num1>num2){
+        if(num1>num3){
+            console.log("num1 is greatest");
+        }
+        else{
+            console.log("num3 is greatest");
+        }
+
+    }
+    else{
+        if(num2>num1){
+            console.log("num2 is greatest");
+        }
+        else{
+            console.log("num3 is greatest");
+        }
+    }
+}
+whichGreater(2, 3, 4)*/
