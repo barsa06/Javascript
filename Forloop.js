cecc3d8c5f12589357ee78c935953d50484befb2
unknown <bpun7891@gmail.com>
2025-11-26 15:56:17 +0545

    added this function

--- Forloop.js
+++ Forloop.js
@@ -1,6 +1,8 @@
+function naturalNum(n){
 let sum = 0;
-    for(let i=0; i<=5; i++){
-        console.log(sum);
+    for(let i=1; i<=n; i++){
         sum= sum + i;
-
     }
+    return sum;
+}
+console.log("Sum of n natural number is", sum(5));
---
+++ ThisFunction.js
@@ -0,0 +1,9 @@
+const isGreatest = (a,b) => {
+  if(a>b){
+    console.log("a is greatest");
+  }
+  else{
+    console.log("b is greatest");
+  }
+}
+isGreatest(3,4);
