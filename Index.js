28b4e914cbfce5980ffe5aa62285dca38e705c13
barsa06 <bpun7891@gmail.com>
2025-12-02 08:41:39 +0545

    added homework

--- Index.js
+++ Index.js
@@ -1,43 +1,7 @@
-//variable declaration
-/*let name = "Rubi";
-name="sulisha";
-console.log(name);
-
-//array 
-const fruits = ["Apple", "Banana", "Carrot"];
-console.log(fruits);
-
-//objects
-const person = {
-    firstName: "John";
-}
-    */
-
-function isEven(a){
-      if (a%2==0) {
-        console.log("Even");
-      } else {
-        console.log("odd");
-      }
-}
-isEven(10)
-
-
-function whichGreater(num1, num2){
-    if (num1>num2){
-        console.log("Number 1 is greatest");
-    }
-    else {
-        console.log("Number 2 is greatest");
-    }
-
-}
-whichGreater(2,3)
-
-/*function whichGreater(num1, num2, num3) {
+function whichGreater(num1, num2, num3) {
     if(num1>num2){
-        if(num1>num3){
-            console.log("num1 is greatest");
+        if(num2>num3){
+            console.log("num2 is greatest");
         }
         else{
             console.log("num3 is greatest");
@@ -45,7 +9,7 @@ whichGreater(2,3)
 
     }
     else{
-        if(num2>num1){
+        if(num2>num3){
             console.log("num2 is greatest");
         }
         else{
@@ -53,4 +17,4 @@ whichGreater(2,3)
         }
     }
 }
-whichGreater(2, 3, 4)*/
+whichGreater(7, 8, 9)
